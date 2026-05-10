306f105b103fd15c90f21d05a22b6b1eaddd233d
Babogaeguri <psyun0323@gmail.com>
2026-05-10 15:39:39 +0900

    update

---
+++ IDE_shower_calculator/main.py
@@ -0,0 +1,14 @@
+from operations.basic import add, subtract, multiply, divide
+from operations.advanced import power, sqrt, factorial
+
+
+print("=== 기본 연산 ===")
+print(add(10, 5))
+print(subtract(10, 5))
+print(multiply(10, 5))
+print(divide(10, 5))
+
+print("=== 고급 연산 ===")
+print(power(2, 10))
+print(sqrt(16))
+print(factorial(5))
